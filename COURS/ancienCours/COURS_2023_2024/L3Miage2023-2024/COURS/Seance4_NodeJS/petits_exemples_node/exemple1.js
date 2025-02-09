27431536e7eb6a18a74cb61a57841f7a3c943c0b
clarator <116170772+clarator@users.noreply.github.com>
2025-02-09 23:18:22 +0100

    modif

--- COURS/ancienCours/COURS_2023_2024/L3Miage2023-2024/COURS/Seance4_NodeJS/petits_exemples_node/exemple1.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-//Création du serveur 
-var server = http.createServer((req, res) => { 
-    res.writeHead(200, {"Content-Type": "text/plain"}); 
-    res.end('Hello World '); 
-});
-//Démarrage du serveur sur le port 8085
-server.listen(8085);
--- COURS/ancienCours/COURS_2023_2024/L3Miage2023-2024/README.md
+++
@@ -1,2 +0,0 @@
-# L3Miage2023-2024
-Repository du cours Intro JavaScript pour les L3 Miage 2024-2024
